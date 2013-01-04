34a622e646a7c8a7eeadf3eb6b41b6c5e72ccb0a
Saurabh <ssaurabh.gne@gmail.com>
2013-01-04 23:02:47 +0530

    Lamp Installtion

---
+++ lamp.sh
@@ -0,0 +1,13 @@
+     sudo apt-get install apache2
+     sudo apt-get install php5 libapache2-mod-php5
+     sudo apt-get install mysql-server mysq-server
+     sudo apt-get install mysql-server mysql-server
+     sudo apt-get install phpmyadmin
+     sudo apt-get install libapache2-mod-auth-mysql php5-mysql
+     sudo /etc/init.d/apache2 restart
+     sudo a2enmod rewrite
+     apache2 restart
+     service apache2 restart
+     sudo /etc/init.d/apache2 restart
+
+
